fb18c11fd7292ebbedbab738cd4d0bc28004f850
jini_11 <jieun21124@naver.com>
2022-09-12 17:50:42 +0900

    first commit

--- main.js
+++
@@ -1,9 +0,0 @@
-import * as posenet from '@tensorflow-models/posenet';
-
-const imageScaleFactor = 0.50;
-const flipHorizontal = false;
-const outputStride = 16;
-const imageElement = document.getElementById('cat');
-// load the posenet model
-const net = await posenet.load();
-const pose = await net.estimateSinglePose(imageElement, scaleFactor, flipHorizontal, outputStride);
